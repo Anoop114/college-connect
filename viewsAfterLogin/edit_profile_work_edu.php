4359b2fec105693d54b789a23f751f017d2b978d
AnujKumar0204 <38970239+AnujKumar0204@users.noreply.github.com>
2021-03-23 13:03:18 +0530

    Update edit_profile_work_edu.php

--- viewsAfterLogin/edit_profile_work_edu.php
+++ viewsAfterLogin/edit_profile_work_edu.php
@@ -1,51 +1,101 @@
             <!-- Edit Work and Education
               ================================================= -->
+                <!-- Education Mode!
+                  ================================================= -->
               <div class="edit-profile-container">
                 <div class="block-title">
                   <h4 class="grey"><i class="icon ion-ios-book-outline"></i>My education</h4>
                   <div class="line"></div>
                 </div>
                 <div class="edit-block">
-                  <form name="education" id="education" class="form-inline" method = "POST">
-                    <div class="row">
-                      <div class="form-group col-xs-12">
-                        <label for="school">My university/institution</label>
-                        <input id="school" class="form-control input-group-lg" type="text" name="school" title="Enter School" placeholder="My School" value="Harvard Unversity" />
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="form-group col-xs-6">
-                        <label for="date-from">From</label>
-                        <input id="date-from" class="form-control input-group-lg" type="text" name="date_from" title="Enter a Date" placeholder="from" value="2012" />
-                      </div>
-                      <div class="form-group col-xs-6">
-                        <label for="date-to" class="">To</label>
-                        <input id="date-to" class="form-control input-group-lg" type="text" name="date_to" title="Enter a Date" placeholder="to" value="2016" />
-                      </div>
+                  <form name="education" id="educ" class="form-inline" method = "POST">
+                  <div class="row">
+                        <div class="form-group col-xs-12">
+                            <label for="education" class="">Education</label>
+                            <select class="form-control" id="education" name="education">
+                            <option value="education">Education</option>
+                            <option>Secondary/Maticulation/10th</option>
+                            <option>Senior Secondary/Inter/12th</option>
+                            <option>Under Graduate</option>
+                            <option>Post Graduate</option>
+                            </select>
+                        </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-xs-12">
                                 <label for="course" class="">Course</label>
-                                <input id="course" class="form-control input-group-lg" type="text" name="course" title="Enter a Course" placeholder="Course"/>
+                                <select class="form-control" id="course" name="course">
+                                  <option value="course">Course</option>
+                                  <option>Course</option>
+                                  <option>Course</option>
+                                  <option>Course</option>
+                                </select>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group col-xs-12">
-                            <label for="education" class="">Education</label>
-                            <select class="form-control" id="education">
-                            <option value="education">Education</option>
-                            <!--Bhai options tu khud daal liyo 
-                                or frr uske baad is coment ko delete kar diyo-->
-                            <option>2000</option>
-                            <option>2001</option>
-                            <option>2002</option>
-                            <option>2004</option>
-                            </select>
+                      <div class="form-group col-xs-12">
+                        <label for="school">School/College/University Name</label>
+                        <input id="school" class="form-control input-group-lg" type="text" name="school" title="Enter School" placeholder="School/College/University"/>
+                      </div>
+                    </div>
+                    
+                    <div class="row">
+                      <div class="form-group col-xs-12">
+                        <label for="time-period">Time Period</label>
+                      </div>
+                        <div class="form-group col-xs-4">
+                          <select class="form-control" id="from-time-period" name="from-time-period">
+                            <option value="from-year">Year</option>
+                            <option>2016</option>
+                            <option>2017</option>
+                            <option>2018</option>
+                            <option>2019</option>
+                            <option>2020</option>
+                          </select>
+                        </div>
+                        <div class="form-group col-xs-1"><label style="padding-left: 8px; padding-top: 3px; font-size: 15px;">TO</label></div>
+                        
+                        <div class="form-group col-xs-4">
+                          <select class="form-control" id="to-time-period" name="to-time-period">
+                            <option value="to-year">Year</option>
+                            <option>2017</option>
+                            <option>2018</option>
+                            <option>2019</option>
+                            <option>2020</option>
+                            <option>2021</option>
+                          </select>
                         </div>
                     </div>
-                    <button class="btn btn-primary">Save & Add</button>
+                    
+                    <button class="btn btn-primary" type="submit" name="save_edu">Save & Add</button>
                   </form>
-                </div>
+                  </div>
+
+                  <?php
+
+
+    if(isset($_POST['save_edu'])){
+
+      $id = $user['clg_id'];
+      
+    $query = "INSERT INTO edu (`clg_id`, `edu_level`, `course`, `inst_name`, `add_year`, `pass_year`)
+              VALUES ('".mysqli_real_escape_string($link, $id)."',
+                      '".mysqli_real_escape_string($link, $_POST['education'])."',
+                      '".mysqli_real_escape_string($link, $_POST['course'])."',
+                      '".mysqli_real_escape_string($link, $_POST['school'])."',
+                      '".mysqli_real_escape_string($link, $_POST['from-time-period'])."',
+                      '".mysqli_real_escape_string($link, $_POST['to-time-period'])."')";
+
+    mysqli_query($link, $query);
+
+}
+
+?>
+
+
+                  <!-- Work Mode!
+                    ================================================= -->  
+                
                 <div class="block-title">
                   <h4 class="grey"><i class="icon ion-ios-briefcase-outline"></i>Work Experiences</h4>
                   <div class="line"></div>
